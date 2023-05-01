f1fbbea9884ecdecceda660d11c5184e77dd9f5c
yasminetak <takatart2001@gmail.com>
2023-05-01 14:57:04 +0100

    Model training and testing2

--- model.py
+++ model.py
@@ -4,11 +4,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# load the data set 
+# Load the data set 
 ratings_data = pd.read_csv('C:/Users/dell/Desktop/Master2022-2023/S2/Machine Learning/Projet Recommender system/ml-latest-small/ratings.csv')
 movies_data = pd.read_csv('C:/Users/dell/Desktop/Master2022-2023/S2/Machine Learning/Projet Recommender system/ml-latest-small/movies.csv')
-# explore the data 
 print(" -Rating data : \n " , ratings_data.head())
 print(" -Movies data : \n " , movies_data.head())
 # drop irrelevant columns like timestamp because it is not required to build the recommender system 
@@ -19,19 +17,39 @@ ratings_data.dropna(inplace=True)
 # check for duplicates and remove em
 print("numbers of duplications : ", ratings_data.duplicated().sum())
 ratings_data.drop_duplicates(inplace=True)
-# merge the dataset, merge the ratings_data and movies_data dataframes
+# number of users and movies
+unique_user = ratings_data.userId.nunique(dropna = True)
+unique_movie = ratings_data.movieId.nunique(dropna = True)
+print("number of unique user:")
+print(unique_user)
+print("number of unique movies:")
+print(unique_movie)
+
+# Merge the dataset, merge the ratings_data and movies_data dataframes
 merged_data = pd.merge(ratings_data, movies_data, on='movieId')
 print("merged data : \n", merged_data.head())
 # finalize the dataset : Drop any remaining irrelevant columns and rename columns if necessary
 merged_data.rename(columns={'title': 'movie_title'}, inplace=True)
 print("new merged data \n", merged_data)
-# save the prepared data in new file
+# save the prepared data in new file prepared_data.csv
 merged_data.to_csv('C:/Users/dell/Desktop/Master2022-2023/S2/Machine Learning/Projet Recommender system/prepared_data.csv', index=False)
 
 # ------------Data Exploration :---------------
 # load the prepared data set 
 data = pd.read_csv('C:/Users/dell/Desktop/Master2022-2023/S2/Machine Learning/Projet Recommender system/prepared_data.csv')
 print("Data : \n", data.head())
+
+# To see the totalRatingCount of movies 
+combine_movie_rating = data.dropna(axis = 0, subset = ['movie_title'])
+movie_ratingCount = (combine_movie_rating.
+     groupby(by = ['movie_title'])['rating'].
+     count().
+     reset_index().
+     rename(columns = {'rating': 'totalRatingCount'})
+     [['movie_title', 'totalRatingCount']]
+    )
+print("Movie ratingCount \n : ",movie_ratingCount.head())
+
 # basic statistics, mean, median
 print(data.describe())
 # plot number of ratings per movie
@@ -47,26 +65,35 @@ plt.xlabel('Rating')
 plt.ylabel('Count')
 plt.title('Distribution of Movies Ratings')
 plt.show()
+# plot rating frequency of each movie(how many time a movie has been rated)
+movie_freq = pd.DataFrame(ratings_data.groupby('movieId').size(),columns=['count'])
+print("how many times a movie have been rated \n :",movie_freq.head())
+# plot movie rating freq
+movie_freq_copy = movie_freq.sort_values(by='count',ascending=False)
+movie_freq_copy=movie_freq_copy.reset_index(drop=True)
+# Creating the plot
+plt.figure(figsize=(12, 8))
+plt.title('Rating Frequency of Movies')
+plt.xlabel('Number of Movies')
+plt.ylabel('Rating Frequency of Movies')
+plt.yscale('log')
+plt.plot(movie_freq_copy['count'])
+plt.show()
 # Identify the most popular movies
 most_popular = data.groupby(['movie_title']).size().sort_values(ascending=False)[:10]
 print("Most popular movies : \n ", most_popular)
-# distribution of genres
-genre_counts = data['genres'].value_counts()
-print("genres : \n ", genre_counts)
-# Sort the DataFrame by ratings in descending order
-sorted_movies = data.sort_values(by='rating', ascending=False)
-# Print the sorted DataFrame
-print("sorted movies by ratings : \n ",sorted_movies)
 
 # ----------Model Implementation with knn: --------------
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import mean_squared_error
+
 # Load preprocessed dataset
 data = pd.read_csv('C:/Users/dell/Desktop/Master2022-2023/S2/Machine Learning/Projet Recommender system/prepared_data.csv')
 print("Data : \n", data.head())
 # Create a pivot table for item-based collaborative filtering
 item_user_matrix = data.pivot_table(index='movieId', columns='userId', values='rating')
+print("The matrix \n :", item_user_matrix.head())
 # Replace missing values with mean
 item_user_matrix = item_user_matrix.apply(lambda x: x.fillna(x.mean()), axis=0)
 # Split the data into training and testing sets
@@ -78,7 +105,7 @@ print("testndata\n : ", test_data)
 k = 10
 model_knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=k)
 model_knn.fit(train_data)
-# Make recommendations for a user
+# # Make recommendations for a user
 user_id = 4 # choose a user ID
 user_ratings = item_user_matrix.loc[:, user_id].values.reshape(1, -1)
 distances, indices = model_knn.kneighbors(user_ratings, n_neighbors=10)
@@ -88,15 +115,14 @@ for i in range(len(indices.flatten())):
     if i == 0:
         print('Recommendations for user {}:'.format(user_id))
     else:
-        movie_id = item_user_matrix.index[indices.flatten()[i]]
-        movie_ids.append(movie_id)
-# Get the movie titles for the recommended movies
-recommended_movies = data[data['movieId'].isin(movie_ids)][['movieId', 'movie_title']]
-recommended_movies = recommended_movies.drop_duplicates(subset='movieId')
-# Print the recommended movies with their IDs and titles
-print(recommended_movies.to_string(index=False))
-
-
+        print('{0}: {1}, with distance of {2}:'.format(i, item_user_matrix.index[indices.flatten()[i]], distances.flatten()[i]))
+#       movie_id = item_user_matrix.index[indices.flatten()[i]]
+#       movie_ids.append(movie_id)
+# # Get the movie titles for the recommended movies
+# recommended_movies = data[data['movieId'].isin(movie_ids)][['movieId', 'movie_title']]
+# recommended_movies = recommended_movies.drop_duplicates(subset='movieId')
+# # # Print the recommended movies with their IDs and titles
+# print(recommended_movies.to_string(index=False))
 
 
 
